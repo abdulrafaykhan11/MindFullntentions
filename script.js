40e42b861ccf9f1a0f2ec6abff5e655689fdd5f7
Abdul Rafay <abdulrafayaptech32@gmail.com>
2026-02-20 16:20:01 +0500

    Improve Poses Section

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 function showTab(tabId, el) {
+  let tabs = document.querySelectorAll(".tab-content");
+  let buttons = document.querySelectorAll(".pose-tabs button");
 
-    let tabs = document.querySelectorAll(".tab-content");
-    let buttons = document.querySelectorAll(".pose-tabs button");
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
 
+  buttons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
 
-    tabs.forEach(tab => {
-        tab.classList.remove("active");
-    });
+  document.getElementById(tabId).classList.add("active");
 
-    buttons.forEach(btn => {
-        btn.classList.remove("active");
-    });
+  el.classList.add("active");
 
-
-    document.getElementById(tabId).classList.add("active");
-
-    el.classList.add("active");
-    
-
+  AOS.refresh();
 }
